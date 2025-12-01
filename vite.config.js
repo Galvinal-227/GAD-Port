da3009aa0e8db1e6bafa627f2f06e16ad54bfb83
GWD <priasetia6282@gmail.com>
2025-12-01 14:17:28 +0700

    vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,13 @@
-// vite.config.js
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  optimizeDeps: {
-    include: ['framer-motion']
+  css: {
+    preprocessorOptions: {
+      scss: {
+        additionalData: `@import "boxicons/css/boxicons.min.css";`
+      }
+    }
   }
 })
